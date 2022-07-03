848e45b7979f0aebd2aaa3b1f9cf937a730612ce
Lukas Rosenthaler <lukas.rosenthaler@unibas.ch>
2022-07-03 02:17:11 +0200

    Started with IIIF

---
+++ handlers/iiifhandler/IiifError.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Lukas Rosenthaler on 03.07.22.
+//
+
+#include "IiifError.h"
+
+namespace cserve {
+} // cserve
---
+++ handlers/iiifhandler/IiifError.h
@@ -0,0 +1,16 @@
+//
+// Created by Lukas Rosenthaler on 03.07.22.
+//
+
+#ifndef CSERVER_IIIFERROR_H
+#define CSERVER_IIIFERROR_H
+
+namespace cserve {
+
+    class IIIFError {
+
+    };
+
+} // cserve
+
+#endif //CSERVER_IIIFERROR_H
