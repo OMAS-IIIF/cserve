d1f4ac6890d6fde4e8c4474f8583278499d07cfd
Lukas Rosenthaler <lukas.rosenthaler@unibas.ch>
2023-03-25 00:54:50 +0100

    ...

--- handlers/iiifhandler/tests/iiiftestserver/scripts/iiifhandlervariables.lua
+++ handlers/iiifhandler/tests/iiiftestserver/scripts/iiifhandlervariables.lua
@@ -2,36 +2,12 @@
 --- Created by rosenth.
 --- DateTime: 10.07.21 22:53
 ---
+require "send_response"
 
 result = {
     status = "OK",
-    config = config,
-
-    server = {
-        method = server.method,
-        has_openssl = server.has_openssl,
-        client_ip = server.client_ip,
-        secure = server.secure,
-        client_port = server.client_port,
-        host = server.host,
-        uri = server.uri,
-        get = server.get,
-        cookies = server.cookies,
-        header = server.header,
-        post = server.post,
-        uploads = server.uploads,
-        content = server.content,
-    },
+    iiifhandler = iiifhandler
 }
 
-local success, jsonresult = server.table_to_json(result)
-if not success then
-server.log("server.table_to_json failed: " .. errmsg, server.loglevel.LOG_ERR)
-server.sendStatus(500)
-print.print("server.table_to_json failed: " .. errmsg)
-return false
-end
-server.sendHeader('Content-type', 'application/json')
-server.sendStatus(200)
-server.print(jsonresult)
+send_success(result)
 return true
